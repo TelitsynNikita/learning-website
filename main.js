99b20dcf5c5af21e2c7fe76afdfaa8a56a744e83
telitsyn nikita <telicyn.nikita@wb.ru>
2025-09-27 13:18:06 +0300

    add

---
+++ main.js
@@ -0,0 +1,52 @@
+const main = document.querySelector('main'),
+      headerNav = document.querySelector('header .navigation__header ul');
+
+document.addEventListener('DOMContentLoaded', () => {
+    headerNav.addEventListener('click', (e) => {
+        switch(e.target.className) {
+            case 'applicants':
+                main.innerHTML = ``;
+                break;
+            case 'baccalaureate':
+                main.innerHTML = `
+                    <section class="main__applicans main__applicans_baccalaureate">
+                        <h2 class="main__applicans_title">Что такое бакалавриат?</h2>
+                        <span>
+                        Бакалавриат — это первая ступень высшего образования, которая предоставляет студентам фундаментальные знания и практические навыки по выбранной специальности. По окончании обучения выпускники получают диплом бакалавра, который открывает широкие возможности для трудоустройства и дальнейшего образования.
+                    </span>
+                    </section>
+                
+                    <section class="main__applicans">
+                        <h2 class="main__applicans_title">Преимущества обучения на бакалавриате</h2>
+                        <ul>
+                            <li>
+                                <span class="title">Оптимальная длительность обучения — 4 года очного обучения</span>
+                            </li>
+                            <li>
+                                <span class="title">Самодостаточное образование — диплом бакалавра признается как законченное высшее образование</span>
+                            </li>
+                            <li>
+                                <span class="title">Широкие карьерные перспективы — возможность трудоустройства по специальности</span>
+                            </li>
+                            <li>
+                                <span class="title">Гибкость выбора — возможность продолжить обучение в магистратуре или начать карьеру</span>
+                            </li>
+                        </ul>
+                    </section>
+                `;
+                break;
+            case 'magistracy':
+                main.innerHTML = 'Магистратура';
+                break;
+            case 'postgraduate':
+                main.innerHTML = 'Аспирантура';
+                break;
+            case 'special':
+                main.innerHTML = 'Средне-специальное';
+                break;
+            case 'about':
+                main.innerHTML = 'О нас';
+                break;
+        }
+    })
+})
