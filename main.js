92bac2aabbf039a5fcf10aa570c38ed4a4de0384
telitsyn nikita <telicyn.nikita@wb.ru>
2025-09-27 13:37:54 +0300

    add

--- main.js
+++ main.js
@@ -5,19 +5,44 @@ document.addEventListener('DOMContentLoaded', () => {
     headerNav.addEventListener('click', (e) => {
         switch(e.target.className) {
             case 'applicants':
-                main.innerHTML = ``;
+                main.innerHTML = `
+                <section class="main__applicans">
+                    <h2 class="main__applicans-title">Почему именно мы?</h2>
+                    <span>
+                        Современный университет открывает двери для амбициозных абитуриентов, готовых к покорению профессиональных вершин. Мы создаем все условия для вашего развития и успешного будущего.
+                    </span>
+                </section>
+                
+                <section class="main__applicans">
+                    <h2 class="main__applicans-title">Что вы получите, поступив к нам?</h2>
+                    <ul>
+                        <li>
+                            <span class="title">Качественное образование от ведущих преподавателей</span>
+                        </li>
+                        <li>
+                            <span class="title">Современную инфраструктуру для учебы и отдыха</span>
+                        </li>
+                        <li>
+                            <span class="title">Практический опыт через стажировки и проекты</span>
+                        </li>
+                        <li>
+                            <span class="title">Карьерные перспективы с поддержкой центра трудоустройства</span>
+                        </li>
+                    </ul>
+                </section>
+                `;
                 break;
             case 'baccalaureate':
                 main.innerHTML = `
-                    <section class="main__applicans main__applicans_baccalaureate">
-                        <h2 class="main__applicans_title">Что такое бакалавриат?</h2>
+                    <section class="main__baccalaureate">
+                        <h2 class="main__baccalaureate_title">Что такое бакалавриат?</h2>
                         <span>
                         Бакалавриат — это первая ступень высшего образования, которая предоставляет студентам фундаментальные знания и практические навыки по выбранной специальности. По окончании обучения выпускники получают диплом бакалавра, который открывает широкие возможности для трудоустройства и дальнейшего образования.
                     </span>
                     </section>
                 
-                    <section class="main__applicans">
-                        <h2 class="main__applicans_title">Преимущества обучения на бакалавриате</h2>
+                    <section class="main__baccalaureate">
+                        <h2 class="main__baccalaureate_title">Преимущества обучения на бакалавриате</h2>
                         <ul>
                             <li>
                                 <span class="title">Оптимальная длительность обучения — 4 года очного обучения</span>
